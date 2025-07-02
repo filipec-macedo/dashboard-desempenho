e410ad91806921ef105b5e253ca0b7fe12f65cab
Filipe Carvalho Macedo <fcarvalho.macedo01@gmail.com>
2025-07-02 17:56:55 -0300

    Ajuste para deploy no Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // necessário para Vercel reconhecer a pasta final
+    outDir: 'dist',
   },
-  server: {
-    port: 3000, // opcional, facilita desenvolvimento local
-  },
-})
+  base: './',
+});
